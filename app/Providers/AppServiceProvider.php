2259595ede13223f58ffacee31e3960c36e8c8db
hirari123 <hirashosanta@gmail.com>
2022-03-31 20:31:51 +0900

    firstCommit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // 自動生成されるリンクのURLをhttpsにする
+        \URL::forceScheme('https');
+    }
+}
